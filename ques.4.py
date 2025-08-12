b60eba705e392bd86e9d41d8394672ef7e08e053
richeekpandey07 <richeekpandey@gmail.com>
2025-08-13 00:47:40 +0530

    ques.5.py

--- ques.4.py
+++
@@ -1,7 +0,0 @@
-correct_password ="python123"
-password =""
-while password != correct_password:
-    password = input("enter the password: ")
-    if password !=correct_password:
-        print ("incorrect password. try again!")
-print("password correct . access granted!")
